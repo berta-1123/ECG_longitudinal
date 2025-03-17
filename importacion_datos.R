f20029faf044cb2e777be78c3c64521f15abfa20
berta ortiz <1632830@uab.cat>
2025-03-17 17:31:47 +0100

    deteccion de picos R

--- importacion_datos.R
+++ importacion_datos.R
@@ -15,4 +15,27 @@ time <- seq(0, (length(ecg)-1)) / fs
 
 plot(time, ecg, type = "l", main="Señal ECG")
 
+#################################
 
+library(pracma)
+library(ggplot2)
+altura_mggplot2altura_min <- 0.5
+dist_min <- fs/2
+picos <- findpeaks (ecg, nups = 1, ndowns = 1, minpeakheight = altura_min, minpeakdistance = dist_min)
+posiciones <- picos[,2]
+amplitudes <- ecg[posiciones]
+
+df_ecg <- data.frame(Tiempo = (1:length(ecg)) / fs, 
+                     Amplitud = ecg)
+
+# Crear un dataframe con los picos R
+df_picos <- data.frame(Tiempo = posiciones / fs, 
+                       Amplitud = amplitudes)
+
+# Graficar la señal ECG con ggplot2
+ggplot() +
+  geom_line(data = df_ecg, aes(x = Tiempo, y = Amplitud), color = "blue") +  # Señal ECG
+  geom_point(data = df_picos, aes(x = Tiempo, y = Amplitud), color = "red", size = 2) +  # Picos R
+  labs(title = "Detección de picos R en la señal ECG",
+       x = "Tiempo (s)", y = "Amplitud (mV)") +
+  theme_minimal()
